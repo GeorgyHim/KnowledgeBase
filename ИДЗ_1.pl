4478fd4875191353251f23b7641b173ffaed1415
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-07 21:12:31 +0300

    Add func for counting for A, B

--- ИДЗ_1.pl
+++ ИДЗ_1.pl
@@ -6,4 +6,12 @@ is_pr(N,I):-Ost is N mod I, Ost > 0, I1 is I+1, is_pr(N,I1).
 
 poly(A, B, N, Res):-Res is N*N+A*N+B.
 
-func(A, B, N):-poly(A, B, N, Res), is_simple(Res).
+func(A, B, N0, Ans):-
+    poly(A, B, N0, Res), is_simple(Res), N1 is N0 + 1, func(A, B, N1, Ans);
+    Ans is N0 - 1, !.
+
+
+
+
+
+%f2 -> ... func(10, 20, 0, Ans)
