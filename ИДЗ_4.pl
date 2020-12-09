c8a8000e11dad88d11adc30257920bd19c5d5639
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-09 17:22:46 +0300

    Temporary commit, trying to fix match

--- ИДЗ_4.pl
+++ ИДЗ_4.pl
@@ -1,3 +1,6 @@
+:- dynamic гул€ет/2.
+:- dynamic не_гул€ет/2.
+
 пет€.
 лена.
 тан€.
@@ -9,13 +12,25 @@
 женщина(лена).
 женщина(тан€).
 
-не_любит(пет€, кошка).
+не_гул€ет(пет€, кошка).
 
 хоз€ин_женщина(хом€к).
 
 гул€ет(тан€, кошка).
 
-match(Kid):-гул€ет(Kid, Pet), write(Kid), write(" - "), write(Pet), !.
+can_walk(Kid, Pet):-не_гул€ет(Kid, Pet), !, fail.
+can_walk(Kid, Pet):-хоз€ин_женщина(Pet), not(женщина(Kid)), !, fail.
+can_walk(_, _):-!.
+
+match(Kid):-гул€ет(Kid, Pet), assertz((не_гул€ет(Kid1, Pet):-Kid1 \= Kid)),!.
+match(Kid):-не_гул€ет(Kid, Pet1), не_гул€ет(Kid, Pet2), Pet1 \= Pet2, assertz((гул€ет(Kid, Pet):-Pet \= Pet1, Pet \= Pet2)),!.
+match(Kid):-write("Wait, "), write(Kid), !.
 
 
+match_kids:-гул€ет(пет€, _), гул€ет(лена, _), гул€ет(тан€, _), !.
+match_kids:-match(пет€), match(лена), match(тан€), match_kids.
+
+answer:-match_kids, гул€ет(пет€, PetP), write(пет€), write(" - "), write(PetP), nl,
+                    гул€ет(лена, PetL), write(лена), write(" - "), write(PetL), nl,
+                    гул€ет(тан€, PetT), write(тан€), write(" - "), write(PetT), !.
 
