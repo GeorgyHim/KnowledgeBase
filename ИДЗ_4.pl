d0e547e2be64221e8ef14d339fe74e6d68f6d7b6
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-09 21:38:06 +0300

    Temp commit with ne_gulaet

--- ИДЗ_4.pl
+++ ИДЗ_4.pl
@@ -18,19 +18,22 @@
 
 гуляет(таня, кошка).
 
-can_walk(Kid, Pet):-не_гуляет(Kid, Pet), !, fail.
-can_walk(Kid, Pet):-хозяин_женщина(Pet), not(женщина(Kid)), !, fail.
-can_walk(_, _):-!.
+can_walk(Kid, Pet, A):-не_гуляет(Kid, Pet), append([], [], A), !.
+can_walk(Kid, Pet, A):-хозяин_женщина(Pet), not(женщина(Kid)), append([], [], A).
+can_walk(_, Pet, A):-append([Pet], [], A).
 
-match(Kid):-гуляет(Kid, Pet), assertz((не_гуляет(Kid1, Pet):-Kid1 \= Kid)),!.
-match(Kid):-не_гуляет(Kid, Pet1), не_гуляет(Kid, Pet2), Pet1 \= Pet2, assertz((гуляет(Kid, Pet):-Pet \= Pet1, Pet \= Pet2)),!.
+find_for(Kid, A):-can_walk(Kid, собака, A1), can_walk(Kid, кошка, A2),  can_walk(Kid, хомяк, A3), append(A1, A2, ATem), append(ATem, A3, A).
+
+match(Kid):-гуляет(Kid, Pet), assertz((не_гуляет(Kid1, Pet):-Kid1 \= Kid)), write(Kid), write(Pet),!.
+match(Kid):-find_for(Kid, A), length(A, 1), assertz((гуляет(Kid, Pet))), write(Kid), write(Pet), !.
 match(Kid):-write("Wait, "), write(Kid), !.
-
 
 match_kids:-гуляет(петя, _), гуляет(лена, _), гуляет(таня, _), !.
 match_kids:-match(петя), match(лена), match(таня), match_kids.
 
-answer:-match_kids, гуляет(петя, PetP), write(петя), write(" - "), write(PetP), nl,
-                    гуляет(лена, PetL), write(лена), write(" - "), write(PetL), nl,
-                    гуляет(таня, PetT), write(таня), write(" - "), write(PetT), !.
+answer:- assertz((не_гуляет(Kid1, кошка):-Kid1 \= таня)),
+    match_kids,
+    гуляет(петя, PetP), write(петя), write(" - "), write(PetP), nl,
+    гуляет(лена, PetL), write(лена), write(" - "), write(PetL), nl,
+    гуляет(таня, PetT), write(таня), write(" - "), write(PetT), !.
 
