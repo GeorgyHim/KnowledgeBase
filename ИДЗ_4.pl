cacd36d1f8012e53522285ca6f959b15983983fb
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-13 17:10:12 +0300

    Improve idz 4

--- ИДЗ_4.pl
+++ ИДЗ_4.pl
@@ -1,22 +1,11 @@
 :- dynamic гул€ет/2.
 
-пет€.
-лена.
-тан€.
-
-собака.
-кошка.
-хом€к.
-
 женщина(лена).
 женщина(тан€).
 
 хоз€ин_женщина(хом€к).
 
 гул€ет(тан€, кошка).
-
-first_el([H|_], H).
-
 can_walk(Kid, Pet, []):-гул€ет(X, Pet), X \= Kid, !.
 can_walk(Kid, Pet, []):-хоз€ин_женщина(Pet), not(женщина(Kid)).
 can_walk(_, Pet, [Pet]).
@@ -24,7 +13,7 @@ can_walk(_, Pet, [Pet]).
 find_for(Kid, Res):-can_walk(Kid, собака, A1), can_walk(Kid, кошка, A2),  can_walk(Kid, хом€к, A3), append(A1, A2, ATem), append(ATem, A3, Res).
 
 match(Kid):-гул€ет(Kid, _), !.
-match(Kid):-find_for(Kid, A), length(A, 1), first_el(A, Pet), assertz((гул€ет(Kid, Pet))), !.
+match(Kid):-find_for(Kid, [Pet]), assertz((гул€ет(Kid, Pet))), !.
 match(_):-!.
 
 match_kids:-гул€ет(пет€, _), гул€ет(лена, _), гул€ет(тан€, _), !.
