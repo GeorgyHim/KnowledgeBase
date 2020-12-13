8244b6f422aeb7775331388ec3cf446f78143bdf
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-13 20:56:17 +0300

    Fix bug in b_a_w. Works now

--- ИДЗ_5.pl
+++ ИДЗ_5.pl
@@ -12,20 +12,17 @@ count_l(L, [_|T], TRes, Res):-count_l(L, T, TRes, Res).
 check_perm(Perm, K):-
     count_l(97, Perm, ResA), count_l(98, Perm, ResB), count_l(99, Perm, ResC),
     count_l(100, Perm, ResD), count_l(101, Perm,ResE), count_l(102, Perm, ResF),
-    Res = [ResA, ResB, ResC, ResD, ResE, ResF], check_perm2(Res, K), write_str(Perm), nl.
-check_perm2(Res, K):-count_l(5, Res, C5), C5 = 1, count_l(K, Res, Ck), Ck is 1.
+    Res = [ResA, ResB, ResC, ResD, ResE, ResF], check_perm2(Res, K), write_str(Perm), nl, !.
+check_perm2(Res, K):-count_l(5, Res, C5), C5 = 1, count_l(K, Res, Ck), Ck = 1.
 
 b_a_w(A, 0, K, PermRes):-check_perm(PermRes, K), !, fail.
-b_a_w(A, N, K, Perm):-in_list(A, El), N1 is N-1, b_a_w(A, N1, K, [El|Perm]).
+b_a_w(A, N, K, Perm):-N > 0, in_list(A, El), N1 is N-1, b_a_w(A, N1, K, [El|Perm]).
 
 build_all_words(_, N, K):-N < K + 5, !, fail. % Слово должно быть не короче чем K+5
 build_all_words(_, N, K):-N > K + 5 + 4, !, fail. % И не длиннее чем K+9
 build_all_words(_, N, 5):-!, fail. % K не должно быть равно 5, иначе 2 буквы встретятся 5 раз
 build_all_words(_, N, K):-K = 1, N >= 7, !, fail. % В этом случае будет 2 буквы по 1 разу
-build_all_words(A, N, K):-
-    tell('C:/Химшиашвили/Учеба/Базы знаний/Задания ProLog/ИДЗ/output_5.txt'),
-    b_a_w(A, N, K,[]),
-    told.
+build_all_words(A, N, K):-tell('C:/Химшиашвили/Учеба/Базы знаний/Задания ProLog/ИДЗ/output_5.txt'), b_a_w(A, N, K,[]), told.
 
 answer:-read(N), read(K), build_all_words([97, 98, 99, 100, 101, 102], N, K).
 % Массив - это 'abcdef'
